2da0649651e5fc30a6f8012119b89989a6b1fcb8
Jamie G <jamiegrossman@outlook.com>
2017-09-11 17:57:22 +0100

    Create intentschema.js

---
+++ intentschema.js
@@ -0,0 +1,49 @@
+{
+  "intents": [
+    {
+      "intent": "AMAZON.PreviousIntent"
+    },
+    {
+      "intent": "AMAZON.NextIntent"
+    },
+    {
+      "intent": "AMAZON.HelpIntent"
+    },
+    {
+      "intent": "AMAZON.ScrollUpIntent"
+    },
+    {
+      "intent": "AMAZON.ScrollLeftIntent"
+    },
+    {
+      "intent": "AMAZON.ScrollDownIntent"
+    },
+    {
+      "intent": "AMAZON.ScrollRightIntent"
+    },
+    {
+      "intent": "AMAZON.PageUpIntent"
+    },
+    {
+      "intent": "AMAZON.PageDownIntent"
+    },
+    {
+      "intent": "AMAZON.MoreIntent"
+    },
+    {
+      "intent": "AMAZON.NavigateSettingsIntent"
+    },
+    {
+      "intent": "AMAZON.StopIntent"
+    },
+    {
+      "slots": [
+        {
+          "name": "fruitName",
+          "type": "FRUIT_TYPES"
+        }
+      ],
+      "intent": "AnswerIntent"
+    }
+  ]
+}
